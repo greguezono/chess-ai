d45f88fda9b22a4823eaee1f922c73c7fe5cd2e0
Greg Uezono <greg.uezono@gmail.com>
2023-10-26 17:17:54 -0300

    Add stockfish api

---
+++ api/stockfish.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://stockfish.online/api/stockfish.php?"
+
+fen = 'r2q1rk1/ppp2ppp/3bbn2/3p4/8/1B1P4/PPP2PPP/RNB1QRK1 w - - 5 11'
+depth = '5'
+mode = 'eval'
+
+endpoint = url + 'fen=' + fen + '&depth=' + depth + '&mode=' + mode
+
+response = requests.get(endpoint)
+print(response.text)
